d1766330509373e3957faba6bc3a9bedd361508a
María <mlovatonv@gmail.com>
2019-10-10 11:56:43 -0500

    Added isEnd method

--- utec/grupo3/Sudoku.java
+++ utec/grupo3/Sudoku.java
@@ -1,5 +1,15 @@
 package utec.grupo3;
 
 public class Sudoku{
-
+  public boolean isEnd() {
+    for (int col = 0; col < 9; ++col) {
+      for (int row = 0; row < 9; ++row) {
+        if (board[col][row] == 0)
+        {
+          return false;
+        }
+      }
+    }
+    return true;
+  }
 }
